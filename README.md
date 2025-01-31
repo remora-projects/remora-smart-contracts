863e4c4fa3ad44b474e716151749a21eb226d387
Ishan Tyagi <ishantyagi42@gmail.com>
2025-01-31 16:14:53 -0500

    clean up

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # remora-smart-contracts
+
+Smart contracts are in contracts folder.
+contracts in contracts/ForTestingOnly/ are for testing purposes only.
+
+To run tests, run:
+'npx hardhat test [path to test]'
+
+Test are run on hardhat local network.
+
+Documentation:
+https://docs.google.com/document/d/1uX3Q-SGbrFLbpaiuKCsQEWg-BEWyMfjyzKKU-JTaXq8/edit?usp=sharing
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,45 +8,11 @@ const config: HardhatUserConfig = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 1000,
+        runs: 100000, //1000,
       },
-    },
-  },
-  zksolc: {
-    version: "1.5.7", // Ensure version is 1.5.7!
-    settings: {
-      // Note: This must be true to call NonceHolder & ContractDeployer system contracts
-      optimizer: {
-        enabled: true,
-        runs: 1000,
-      },
-      enableEraVMExtensions: false,
     },
   },
   defaultNetwork: "hardhat",
-  networks: {
-    abstractTestnet: {
-      url: "https://api.testnet.abs.xyz",
-      ethNetwork: "sepolia",
-      zksync: true,
-      chainId: 11124,
-    },
-  },
-  etherscan: {
-    apiKey: {
-      abstractTestnet: "TACK2D1RGYX9U7MC31SZWWQ7FCWRYQ96AD",
-    },
-    customChains: [
-      {
-        network: "abstractTestnet",
-        chainId: 11124,
-        urls: {
-          apiURL: "https://api-sepolia.abscan.org/api",
-          browserURL: "https://sepolia.abscan.org/",
-        },
-      },
-    ],
-  },
 };
 
 export default config;
